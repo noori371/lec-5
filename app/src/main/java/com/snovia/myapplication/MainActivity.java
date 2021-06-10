6f91102d4743dfa2eda4dd7c0c45e959dd2c9e9b
Nooria <bsef18a041@pucit.edu.pk>
2021-06-10 11:55:51 +0500

    v

---
+++ app/src/main/java/com/snovia/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.snovia.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button2);
+        textView= findViewById(R.id.textView3);
+    }
+
+
+    public void WhoIsHere(View view) {
+        textView.setText("I am here");
+    }
+}
